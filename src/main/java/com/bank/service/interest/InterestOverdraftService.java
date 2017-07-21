178f990f4a6dd89bd36d78627ef5c4ccd6651a5e
boschma2702 <reneboschma@hotmail.com>
2017-07-21 14:48:50 +0200

    Added logging to interestServices

--- src/main/java/com/bank/service/interest/InterestOverdraftService.java
+++ src/main/java/com/bank/service/interest/InterestOverdraftService.java
@@ -4,6 +4,7 @@ import com.bank.bean.account.AccountBean;
 import com.bank.repository.account.AccountRepository;
 import com.bank.service.time.TimeService;
 import com.bank.util.DayPassedListener;
+import com.bank.util.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,6 +32,7 @@ public class InterestOverdraftService implements DayPassedListener {
     @Override
     @Transactional
     public void onDayPassed(Date start, Date end) {
+        Logger.info("Calculating overdraft interest over day");
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(start);
         int amountOfDaysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
@@ -38,6 +40,7 @@ public class InterestOverdraftService implements DayPassedListener {
         for(AccountBean accountBean : accountBeans){
             double buildUpInterest = accountBean.getBuildUpOverdraftInterest();
             double interest = getInterest(amountOfDaysInMonth, accountBean.getMinimumDayAmount());
+            Logger.info("Overdraft interest of accountId=%s is interest=%s", accountBean.getAccountId(), interest);
             accountBean.setBuildUpOverdraftInterest(buildUpInterest+interest);
             accountRepository.save(accountBean);
         }
--- src/main/java/com/bank/service/interest/InterestOverdraftTransferService.java
+++ src/main/java/com/bank/service/interest/InterestOverdraftTransferService.java
@@ -2,6 +2,7 @@ package com.bank.service.interest;
 
 import com.bank.bean.account.AccountBean;
 import com.bank.repository.account.AccountRepository;
+import com.bank.util.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +16,14 @@ public class InterestOverdraftTransferService {
     private AccountRepository accountRepository;
 
     public void transferOverdraftInterest(){
+        Logger.info("Transferring overdraft interest");
         List<AccountBean> accountBeans = accountRepository.getActiveAccountBeansWithOverdraftInterest();
         for(AccountBean accountBean : accountBeans){
             BigDecimal decimal = new BigDecimal(accountBean.getBuildUpOverdraftInterest());
             decimal = decimal.setScale(2, BigDecimal.ROUND_HALF_UP);
             accountBean.setAmount(accountBean.getAmount()+decimal.doubleValue());
             accountBean.setBuildUpOverdraftInterest(0);
+            Logger.info("Overdraft interest amount=%s transferred to accountId=%s", decimal.doubleValue(), accountBean.getAccountId());
             accountRepository.save(accountBean);
         }
     }
