008e750d65da05e14822a515c85a2c00fa5a29bd
boschma2702 <reneboschma@hotmail.com>
2017-07-21 14:38:14 +0200

    Added logging to customerServices

--- src/main/java/com/bank/service/customer/CustomerService.java
+++ src/main/java/com/bank/service/customer/CustomerService.java
@@ -2,6 +2,7 @@ package com.bank.service.customer;
 
 import com.bank.bean.customer.CustomerBean;
 import com.bank.repository.customer.CustomerRepository;
+import com.bank.util.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,12 @@ public class CustomerService {
     private CustomerRepository customerRepository;
 
     public CustomerBean getCustomerBeanById(int customerId) {
+        Logger.info("Retrieving CustomerBean from customerId=%s", customerId);
         return customerRepository.getCustomerBeanByCustomerId(customerId);
     }
 
     public CustomerBean getCustomerBeanByUsername(String username) {
+        Logger.info("Retrieving CustomerBean from username=%s", username);
         return customerRepository.getCustomerBeanByUsername(username);
     }
 }
