c6d630bc2fca93341d4b62379681cbeaa4d442aa
boschma2702 <reneboschma@hotmail.com>
2017-07-21 14:52:23 +0200

    Added logging to sessionServices

--- src/main/java/com/bank/service/session/SessionLoginService.java
+++ src/main/java/com/bank/service/session/SessionLoginService.java
@@ -6,6 +6,7 @@ import com.bank.projection.session.SessionPasswordProjection;
 import com.bank.repository.session.LoginRepository;
 import com.bank.service.AuthenticationService;
 import com.bank.service.customer.CustomerGetService;
+import com.bank.util.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,16 +19,19 @@ public class SessionLoginService {
     private CustomerGetService customerGetService;
 
     private boolean checkLogin(String username, String password) {
+        Logger.info("Checking login of username=%s", username);
         SessionPasswordProjection sessionPasswordProjection = loginRepository.findByUsername(username);
         return sessionPasswordProjection != null && sessionPasswordProjection.getPassword() != null && sessionPasswordProjection.getPassword().equals(password);
     }
 
     public SessionAuthTokenProjection getAuthToken(String username, String password) throws AuthenticationException {
+        Logger.info("Retrieving authentication token of username=%s", username);
         if (checkLogin(username, password)) {
             SessionAuthTokenProjection projection = new SessionAuthTokenProjection();
             projection.setAuthToken(AuthenticationService.instance.login(customerGetService.getCustomerId(username)));
             return projection;
         } else {
+            Logger.error("Invalid username and password combination of username=%s", username);
             throw new AuthenticationException("Invalid username password combination");
         }
     }
