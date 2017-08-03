5ded129e7951b49e50a05cdc91b93d696d117ecd
boschma2702 <reneboschma@hotmail.com>
2017-08-03 21:43:16 +0200

    added credit card block after 3 attempts

--- src/main/java/com/bank/service/creditcard/CreditCardValidateService.java
+++ src/main/java/com/bank/service/creditcard/CreditCardValidateService.java
@@ -3,6 +3,8 @@ package com.bank.service.creditcard;
 import com.bank.bean.creditcard.CreditCardBean;
 import com.bank.exception.InvalidPINException;
 import com.bank.exception.InvalidParamValueException;
+import com.bank.repository.creditcard.CreditCardRepository;
+import com.bank.util.Constants;
 import com.bank.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,28 @@ public class CreditCardValidateService {
     @Autowired
     private CreditCardService creditCardService;
 
+    @Autowired
+    private CreditCardRepository creditCardRepository;
+
     public CreditCardBean validateCreditCard(String creditCardNumber, String pinCode) throws InvalidParamValueException, InvalidPINException {
         Logger.info("Validating creditCardNumber=%s", creditCardNumber);
         CreditCardBean creditCardBean = creditCardService.getCreditCardBeanByCreditCardNumber(creditCardNumber, true);
-        if(!creditCardBean.getCreditCardPin().equals(pinCode)){
+
+        if(creditCardBean.getCreditCardPin().endsWith(pinCode)){
+            if(creditCardBean.getAttempts()!=0){
+                creditCardBean.setAttempts(0);
+                creditCardRepository.save(creditCardBean);
+            }
+        }else{
+            if(creditCardBean.getAttempts() == Constants.CARD_BLOCK_LIMIT){
+                Logger.info("Attempting to pay with blocked credit card, creditCardId=%s", creditCardBean.getCreditCardId());
+            }else{
+                creditCardBean.setAttempts(creditCardBean.getAttempts()+1);
+                if(creditCardBean.getAttempts() == Constants.CARD_BLOCK_LIMIT){
+                    creditCardBean.setActive(false);
+                }
+            }
+            creditCardRepository.save(creditCardBean);
             Logger.error("Invalid combination of creditCardId=%s and pinCode", creditCardBean.getCreditCardId());
             throw new InvalidPINException("Invalid credit card number and pincode");
         }
