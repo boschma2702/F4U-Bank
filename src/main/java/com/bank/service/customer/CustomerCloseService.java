b81436c7c8b65b1b28d5cfbcec52b116a9d21caa
boschma2702 <reneboschma@hotmail.com>
2017-08-23 14:54:52 +0200

    fixed customer close

--- src/main/java/com/bank/service/customer/CustomerCloseService.java
+++ src/main/java/com/bank/service/customer/CustomerCloseService.java
@@ -1,6 +1,7 @@
 package com.bank.service.customer;
 
 import com.bank.repository.customer.CustomerRepository;
+import com.bank.repository.person.PersonRepository;
 import com.bank.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,12 @@ public class CustomerCloseService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private PersonRepository personRepository;
+
     public void closeCustomer(int customerId) {
         Logger.info("Closing customerId=%s", customerId);
         customerRepository.closeCustomer(customerId);
+        personRepository.deletePersonByCustomerId(customerId);
     }
 }
